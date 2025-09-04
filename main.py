fe80df25420cec2cc46a3200f782d08546480e38
Ludwig Wittenberg <38007788+LudwigWittenberg@users.noreply.github.com>
2025-09-04 12:45:18 +0200

    Refactored all
    Signed-off-by: Ludwig Wittenberg <38007788+LudwigWittenberg@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from src.tasks.task1 import task1
+from src.tasks.task1 import Task1
 
 def main():
-  task1()
+  print("----------- Task 1 -----------")
+
+  t1 = Task1(10)
+
+  t1.add_union(1, 2)
+  t1.add_union(2, 3)
+  t1.is_connected(1, 3)
+
+  print("")
+  print("----------- Task 2 -----------")
 
 main()
 
--- src/tasks/task1.py
+++ src/tasks/task1.py
@@ -2,36 +2,24 @@ from src.printHelper import PrintHelper
 from src.utils.UnionFind import UnionFind
 import time
 
-def task1():
-  printHelper = PrintHelper()
+class Task1:
+  def __init__(self, num):
+    self.uf = UnionFind(num)
 
- # -------------------- TASK 1 --------------------
-  printHelper.new_task("1")
+  # uf = UnionFind(10)
+  # print_union(uf, 3, 5)
 
-  uf = UnionFind(10)
-  print_union(uf, 3, 5)
+  # print_connected(uf, 3, 5)
 
-  print_connected(uf, 3, 5)
+  def add_union(self, num1, num2):
+    print("")
+    print(num1, "unioned with", num2)
+    self.uf.union(num1, num2)
 
-# PRINT HELPERS
+  def is_connected(self, num1, num2):
+    is_connected = self.uf.connected(num1, num2)
 
-def print_connected(uf, num1, num2):
-  # Calculate time for operation
-  tic = time.perf_counter()
-  is_connected = uf.connected(num1, num2)
-  toc = time.perf_counter()
+    print("")
+    print("Is", num1, "connected to", num2, "?", is_connected)
 
-  print("Is", num1, "connected to", num2, "?", is_connected)
-  print(f"Connected operation took {toc - tic:0.4f} seconds")
-  print("")
-
-def print_union(uf, num1, num2):
-  print(num1, "unioned with", num2)
-
-  # Calculate time for operation
-  tic = time.perf_counter()
-  uf.union(num1, num2)
-  toc = time.perf_counter()
-
-  print(f"Union operation took {toc - tic:0.4f} seconds")
-  print("")
+    return is_connected
